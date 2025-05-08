f7c57dfd833e308db3afec045e60afb50c2b3ade
HuyNM317 <huynm@fullstack.edu.vn>
2025-05-08 11:09:07 +0700

    update code by review

--- js/slide.js
+++ js/slide.js
@@ -1,52 +1,85 @@
 const makeSlideShow = function (className) {
-  return (currentIndex = 0) => {
-    const slideContainer = document.querySelector(
-      `${className} .slide-show__container`
-    );
-    const slideItems = Array.from(slideContainer.children);
-    const slideItemWidth = slideContainer.clientWidth;
+  const slideContainer = document.querySelector(
+    `${className} .slide-show__container`
+  );
+  const slideItems = Array.from(slideContainer.children);
+  const slideItemWidth = slideContainer.clientWidth;
 
-    const prevBtn = document.querySelector(
-      `${className} .slide-show__nav-btn--prev`
-    );
-    const nextBtn = document.querySelector(
-      `${className} .slide-show__nav-btn--next`
-    );
-    const dotList = Array.from(
-      document.querySelector(`${className} .slide-show__dots`).children
-    );
+  const prevBtn = document.querySelector(
+    `${className} .slide-show__nav-btn--prev`
+  );
+  const nextBtn = document.querySelector(
+    `${className} .slide-show__nav-btn--next`
+  );
+  const dotList = Array.from(
+    document.querySelector(`${className} .slide-show__dots`).children
+  );
+  slideContainer.style.width = `${slideItemWidth * slideItems.length}px`;
 
-    slideContainer.style.width = `${slideItemWidth * slideItems.length}px`;
+  let currentIndex;
 
-    let idInterval = setInterval(() => {
-      nextBtn.click();
+  const handleChangeSlide = (index) => {
+    slideContainer.style.transition = null;
+    currentIndex = index;
+    const position = (100 / slideItems.length) * index * -1;
+    slideContainer.style.transform = `translateX(${position}%)`;
+    handleChangeDotActive(currentIndex);
+  };
+
+  const handleChangeDotActive = (indexActive) => {
+    dotList.forEach((dot, index) => {
+      if (
+        dot.children[0].classList.contains("active-bg") &&
+        indexActive !== index
+      ) {
+        dot.children[0].classList.remove("active-bg");
+      } else if (index === indexActive) {
+        dot.children[0].classList.add("active-bg");
+      }
+    });
+  };
+
+  const navigateSlide = (action) => {
+    if (action === "next") {
+      currentIndex++;
+    } else if (action === "prev") {
+      currentIndex--;
+    }
+    if (currentIndex > slideItems.length - 1 || currentIndex < 0) {
+      currentIndex = 0;
+    }
+    handleChangeSlide(currentIndex);
+  };
+
+  let idInterval = setInterval(() => {
+    navigateSlide("next");
+  }, 5000);
+
+  const handleInterval = () => {
+    clearInterval(idInterval);
+    idInterval = setInterval(() => {
+      navigateSlide("next");
     }, 5000);
+  };
 
-    const handleChangeSlide = (index) => {
-      slideContainer.style.transition = null;
-      clearInterval(idInterval);
-      currentIndex = index;
-      const position = (100 / slideItems.length) * index * -1;
-      slideContainer.style.transform = `translateX(${position}%)`;
-      handleChangeDotActive(currentIndex);
-      idInterval = setInterval(() => {
-        nextBtn.click();
-      }, 5000);
-    };
+  nextBtn.addEventListener("click", () => {
+    navigateSlide("next");
+    handleInterval();
+  });
 
-    const handleChangeDotActive = (indexActive) => {
-      dotList.forEach((dot, index) => {
-        if (
-          dot.children[0].classList.contains("active-bg") &&
-          indexActive !== index
-        ) {
-          dot.children[0].classList.remove("active-bg");
-        } else if (index === indexActive) {
-          dot.children[0].classList.add("active-bg");
-        }
-      });
-    };
+  prevBtn.addEventListener("click", () => {
+    navigateSlide("prev");
+    handleInterval();
+  });
 
+  dotList.forEach((dot, index) => {
+    dot.addEventListener("click", function () {
+      handleChangeSlide(index);
+      handleInterval();
+    });
+  });
+  return (index = 0) => {
+    currentIndex = index;
     if (currentIndex !== 0) {
       slideContainer.style.transition = "none";
       slideContainer.style.transform = `translateX(${
@@ -54,29 +87,7 @@ const makeSlideShow = function (className) {
       }px)`;
       handleChangeDotActive(currentIndex);
     }
-
-    nextBtn.addEventListener("click", () => {
-      currentIndex++;
-      if (currentIndex > slideItems.length - 1) {
-        currentIndex = 0;
-      }
-      handleChangeSlide(currentIndex);
-    });
-
-    prevBtn.addEventListener("click", () => {
-      currentIndex--;
-      if (currentIndex < 0) {
-        currentIndex = 0;
-      }
-      handleChangeSlide(currentIndex);
-    });
-
-    dotList.forEach((dot, index) => {
-      dot.addEventListener("click", function () {
-        handleChangeSlide(index);
-      });
-    });
   };
 };
 
-makeSlideShow(".slide-show")();
+makeSlideShow(".slide-show")(0);
